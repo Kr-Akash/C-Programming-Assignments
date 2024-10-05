525a9e849036aeb146ddea93b44dc9e45e29fec8
Kr-Akash <akashkumarkly@gmail.com>
2024-10-05 14:30:17 +0530

    Update Expression.cpp

--- Assignment-5/Expression.cpp
+++ Assignment-5/Expression.cpp
@@ -1,155 +1,121 @@
-#include <iostream>
-#include <bits/stdc++.h>
-#include <stack>
-#include <string>
+#include<iostream>
+#include<stack>
+#include<cstring>
+
 using namespace std;
 
-class Expr
-{
-    // ...
+
+class Expr{
+	
 public:
-    const char *expr;
-    Expr(const char *expr)
-    {
-        this->expr = expr;
-    }
-    // Expr(const char *expr);
-    int eval();   // returns the value of the expression
-    void print(); // prints representation of the expression on cout
+	const char* infix;
+	string postfix;
+	Expr(const char* expression){ 
+	/*
+		constructor that initializes the class variable infix to the expression passed as parameter
+		when creating an instance of this class Expr.
+		infix_to_postfix() function is called and class variable postfix then contains the string that is 
+		postfix notation of the infix expression.
+	*/
+		this->infix = expression;
+		infix_to_postfix();
+	}
+	
+	bool isOperand(char symbol){ //this function returns true if the symbol is a digit
+		 return (symbol >= '0' && symbol <= '9');
+
+	}
+	
+	bool prcd(char a, char b){ 
+	/*this function returns true when the operator a has higher precedence 
+	 than operator b. It returns false otherwise*/
+		return prcd(a) > prcd(b);
+	}
+	
+	int prcd(char op){ //the higher the value the higher precedence
+		if(op == '+'||op == '-')
+    			return 1;
+    		if(op == '*'||op == '/')
+    			return 2;
+    		return 0;
+	}
+	
+	void infix_to_postfix(){ //called everytime when the instance of the class is created
+	//function converts infix notation to postfix notation
+		stack<char> opstk;
+		for(int i = 0; i < strlen(infix); ++i){
+			char symb = infix[i];
+			if(isOperand(symb)){
+				postfix += symb;
+			}
+			else{
+				while(!opstk.empty() && prcd(opstk.top(), symb)){
+					char topSymb = opstk.top();
+					opstk.pop();
+					postfix += topSymb;
+				}
+				opstk.push(symb);
+			}
+		}
+		while(!opstk.empty()){
+			char topSymb = opstk.top();
+			opstk.pop();
+			postfix += topSymb;
+		}
+	
+	}
+	
+	int eval(){
+	/*
+		Function evaluates the postfix notation of the infix expression that was passed while creating 
+		the instance of the class Expr.
+		Returns the evaluated value of the epxression.
+	*/
+		stack<int> stack;
+		for(char symb : postfix){
+			if (isOperand(symb)) {
+			stack.push(symb - '0');
+		} 
+		else
+		{
+			int operand2 = stack.top(); 
+			stack.pop();
+			int operand1 = stack.top();
+			stack.pop();
+
+			switch(symb){
+		        	case '+': stack.push(operand1 + operand2); 
+		        	break;
+		        	case '-': stack.push(operand1 - operand2); 
+		        	break;
+		        	case '*': stack.push(operand1 * operand2); 
+		        	break;
+		        	case '/': stack.push(operand1 / operand2); 
+		        	break;
+		        	default: cout << "Operation not supported! Program terminated!" << endl;
+		        	exit(0);
+			}
+		}
+	    }
+	    return stack.top();
+	}
+	
+	void print(){ //prints the expression in the infix notation 
+		cout << infix << endl;
+		//cout << postfix << endl;
+	}
+
 };
 
-// Function to check the precedence of operators
-int precedence(char op)
-{
-    if (op == '+' || op == '-')
-        return 1;
-    if (op == '*' || op == '/')
-        return 2;
-    return 0;
-}
 
-string infixToPostfix(const char *infix)
-{
-    stack<char> st;
-    string p = "";
-    for (int i = 0; i < strlen(infix); i++)
-    {
-        char c = infix[i];
-
-        // If the scanned character is an operand, add it to
-        // output string.
-        if (isalnum(c))
-        {
-            p += c;
-        }
-
-        // If the scanned character is an '(', push it to
-        // the stack.
-        else if (c == '(')
-            st.push('(');
-
-        // If the scanned character is an ')', pop and to
-        // output string from the stack until an '(' is
-        // encountered.
-        else if (c == ')')
-        {
-            while (st.top() != '(')
-            {
-                p += st.top();
-                st.pop();
-            }
-            st.pop();
-        }
-
-        // If an operator is scanned
-        else
-        {
-            while (!st.empty() && precedence(c) <= precedence(st.top()))
-            {
-                p += st.top();
-                st.pop();
-            }
-            st.push(c);
-        }
-    }
-
-    // Pop all the remaining elements from the stack
-    while (!st.empty())
-    {
-        p += st.top();
-        st.pop();
-    }
-    return p;
-    
-}
-
-int performOperation(int operand1, int operand2, char operation)
-{
-    switch (operation)
-    {
-    case '+':
-        return operand1 + operand2;
-    case '-':
-        return operand1 - operand2;
-    case '*':
-        return operand1 * operand2;
-    case '/':
-        return operand1 / operand2;
-    default:
-        return 0;
-    }
-}
-
-int Expr::eval()
-{
-    const char *infix = expr;
-    string str = infixToPostfix(infix);
-    const char *postfix = &str[0];
-    //cout << "postfix  " << postfix <<  endl;;  
-    stack<int> stack;
-
-    for (int i = 0; i < strlen(postfix); i++)
-    {
-        char c = postfix[i];
-
-        if (isdigit(c))
-        {
-            // Convert char digit to int and push onto the
-            // stack
-            stack.push(c - '0');
-        }
-        else
-        {
-            // Pop the top two elements for the operation
-            int operand2 = stack.top();
-            stack.pop();
-            int operand1 = stack.top();
-            stack.pop();
-
-            // Perform operation and push the result back
-            // onto the stack
-            int result = performOperation(operand1, operand2, c);
-            stack.push(result);
-        }
-    }
-    // The final result should be the only item left in the
-    // stack
-    return stack.top();
-}
-
-void Expr::print()
-{
-    cout << expr << endl;
-}
-
-int main()
-{
-    Expr x("8/4+3*4-3");
-    cout << "x = " << x.eval() << "\n";
-    x.print();
-    
-    Expr y("9+2*3");
-    cout << "y = " << y.eval() << "\n";
-    y.print();
-    return 0;
+int main(void){
+	Expr x("8/4+3*4-3");
+	cout << "x = " << x.eval() << "\n";
+	x.print();
+	
+	Expr y("5+2*5");
+	cout << "y = " << y.eval() << "\n";
+	y.print();
+	
+	return 0;
 }
